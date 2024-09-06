f76a0e8ef198cd27fdfb46e9866923ff29f1f4c7
Cain <134607924+CainC0de@users.noreply.github.com>
2024-09-06 12:36:31 -0300

    Update databankscript.py

--- Data/databankscript.py
+++ Data/databankscript.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-# Conecte-se ao banco de dados (se não existir, será criado)
+
 conexao = sqlite3.connect('dados_do_projeto.db')
 
-# Crie um cursor para executar comandos SQL
+
 cursor = conexao.cursor()
 
-# Crie uma tabela para armazenar os dados dos sensores
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS leituras_sensor (
                     id INTEGER PRIMARY KEY,
                     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -16,5 +16,5 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS leituras_sensor (
                     hora TIME
                 )''')
 
-# Feche a conexão com o banco de dados
+
 conexao.close()
